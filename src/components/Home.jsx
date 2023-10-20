a89dc8efef8196cf4791afae268b356527fea01e
Ruth Mores <moresruthantonette@gmail.com>
2023-10-20 08:41:06 +0800

    fix: home img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,15 +27,15 @@ const Home = () => {
         </a>
       </div>
 
-      <img
+      {/* <img
         src={imgHero}
         alt=""
-        className="mt-[2rem] sm:w-[50%] w-[90%] ml-[1.5rem] sm:flex-1 px-6 py-6"
-      />
+        className="mt-[2rem] sm:w-[100%] w-[90%] ml-[1.5rem] sm:flex-1 px-6 py-6"
+      /> */}
 
-      {/* <div className="sm:w-1/2  px-6 py-6">
+      <div className="sm:w-1/2  px-6 py-6">
         <img src={imgHero} alt="" className="mt-[2rem]" />
-      </div> */}
+      </div>
     </section>
   );
 };
